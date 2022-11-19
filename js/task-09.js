47c1b55ec347b962b7010a425dba58387b9fb677
Anatoliia Riabchenko <108002173+AnatoliiaR@users.noreply.github.com>
2022-11-19 05:57:20 +0200

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const colorBtn = document.querySelector('.change-color');
+const colorSpanEl = document.querySelector('.color');
+
+
+colorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor(event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorSpanEl.textContent = `${getRandomHexColor()}`;
+
+}
+
